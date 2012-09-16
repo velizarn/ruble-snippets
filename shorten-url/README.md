75b352ea34aa934fa90f6449674ba2afd77a6cfd
Velizar Nenov <vndepot-wdg@yahoo.com>
2012-09-17 00:21:43 +0300

    Add Shorten URL command

---
+++ shorten-url/README.md
@@ -0,0 +1,21 @@
+shorten-url
+============
+
+This command creates shortened URLs using http://is.gd/ API
+Takes the input argument as an URL and sends it to cliepboard
+
+http://is.gd/index.php
+http://is.gd/faq.php
+http://is.gd/developers.php
+http://is.gd/apilookupreference.php
+http://is.gd/forward.php?shorturl=http://is.gd/c179El54&format=simple&
+http://is.gd/apishorteningreference.php
+
+This will turns on logging of detailed statistics when the shortened URL you create is accessed
+http://is.gd/create.php?url=https://github.com/&format=simple&logstats=1
+
+Stats URL
+http://is.gd/stats.php?url=SHORTURL
+
+is.gd usage limits
+http://is.gd/usagelimits.php
---
+++ shorten-url/command.rb
@@ -0,0 +1,50 @@
+require 'ruble'
+require 'ruble/ui'
+require 'open-uri'
+require 'net/http'
+
+command 'Shorten URL' do |cmd|
+  cmd.input = :selection
+  cmd.output = :copy_to_clipboard
+  cmd.scope = :all
+  cmd.invoke do |context|
+
+    service_url = 'v.gd'
+
+    selection = ENV['TM_SELECTED_TEXT']
+
+    if (selection.nil? || selection.empty?) then
+      options = {
+        :title => "Enter URL",
+        :prompt => "Enter URL for shortenning",
+        :button1 => "Ok",
+        :button2 => "Cancel",
+      }
+      selection = Ruble::UI.request_string(options)
+      if (selection.nil? || selection.empty?) then
+        Ruble::Logger.log_error "Empty URL string"
+        context.exit_discard()
+      end
+    end
+
+    url = 'http://'+service_url+'/create.php?format=simple&url='+selection
+
+    uri = URI.parse(url)
+    gdResponse = nil
+
+    Net::HTTP.start(uri.host, uri.port) do |http|
+      gdResponse = http.get(uri.request_uri)
+    end
+
+     if (gdResponse.code != '200') then
+       Ruble::Logger.log_error "Error while shortenning the selected URL..."
+       Ruble::Logger.log_error gdResponse.body
+       context.exit_discard()
+     end
+
+     puts gdResponse.body
+
+     Ruble::Logger.log_info "Shortened URL "+gdResponse.body+" has been copied to clipboard"
+
+  end
+end
